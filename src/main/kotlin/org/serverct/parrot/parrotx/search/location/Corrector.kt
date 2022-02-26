9e9f6d6d2aaea1bddf2f8a555616577c0653d4ff
Legoshi <entityparrot@qq.com>
2022-02-26 17:43:46 +0800

    [*] 优化 SurfaceCorrector
    本次 Commit 共包含以下内容:
    - [*] ...

--- src/main/kotlin/org/serverct/parrot/parrotx/search/location/Corrector.kt
+++ src/main/kotlin/org/serverct/parrot/parrotx/search/location/Corrector.kt
@@ -1,6 +1,7 @@
 package org.serverct.parrot.parrotx.search.location
 
 import org.bukkit.Location
+import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.serverct.parrot.parrotx.search.api.corrector.Corrector
 import taboolib.common.platform.function.submit
@@ -8,22 +9,26 @@ import java.util.concurrent.CompletableFuture
 
 object SurfaceCorrector : Corrector<Location> {
 
+    private val heights = 0 until 255
+
     override fun correct(value: Location): CompletableFuture<Location> {
         val future = CompletableFuture<Location>()
         if (value.world == null || value.blockY < 0) {
             future.completeExceptionally(IllegalArgumentException("Invalid location: $value"))
             return future
         }
-
         submit(async = true) {
             var block = value.block
-            while (block.y > 0 && block.isEmpty) {
-                block = block.getRelative(BlockFace.DOWN)
+            while (block.y in heights && !isSurface(block)) {
+                block = closer(block)
             }
             future.complete(block.location)
         }
-
         return future
     }
+
+    private fun isSurface(block: Block): Boolean = !block.isEmpty && block.getRelative(BlockFace.UP).isEmpty
+
+    private fun closer(block: Block): Block = block.getRelative(if (block.isEmpty) BlockFace.DOWN else BlockFace.UP)
 
 }
