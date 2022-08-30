0788e528003914fed7500645d7544270c6639fcb
Legoshi <entityparrot@qq.com>
2022-08-30 10:45:13 +0800

    [+] 新功能(1.4.4): 新增快捷应用 Linked 的 Menu 实现类 Mapped

---
+++ src/main/kotlin/org/serverct/parrot/parrotx/ui/extension/Mapped.kt
@@ -0,0 +1,71 @@
+package org.serverct.parrot.parrotx.ui.extension
+
+import org.bukkit.inventory.Inventory
+import org.serverct.parrot.parrotx.ui.config.MenuConfiguration
+import taboolib.module.ui.Menu
+import taboolib.module.ui.buildMenu
+import taboolib.module.ui.type.Linked
+
+@Suppress("unused")
+class Mapped<E>(title: String) : Menu(title) {
+
+    private lateinit var config: MenuConfiguration
+    private lateinit var template: String
+    private lateinit var elements: () -> Iterable<E>
+    private lateinit var args: Array<out Any?>
+    private var prev: String = "Previous"
+    private var next: String = "Next"
+
+    fun configuration(config: MenuConfiguration) {
+        this.config = config
+    }
+
+    fun template(keyword: String) {
+        this.template = keyword
+    }
+
+    fun argument(vararg args: Any?) {
+        this.args = args
+    }
+
+    fun setPrevPage(keyword: String) {
+        this.prev = keyword
+    }
+
+    fun setNextPage(keyword: String) {
+        this.next = keyword
+    }
+
+    override fun build(): Inventory {
+        return buildMenu<Linked<E>> {
+            val (shape, templates) = config
+            rows(shape.rows)
+            slots(shape[template].toList())
+            elements { elements().toList() }
+
+            val _template = templates.require(template)
+            onGenerate { _, element, index, slot ->
+                _template(slot, index, element, *args)
+            }
+            onClick { event, element ->
+                _template.handle(event, this, element, *args)
+            }
+
+            config.setPreviousPage(this)
+            config.setNextPage(this)
+
+            onBuild {
+                shape.all(template, prev, next) { slot, index, item, _ ->
+                    it.setItem(slot, item(slot, index, *args))
+                }
+            }
+            onClick { event ->
+                event.isCancelled = true
+                if (event.rawSlot in shape) {
+                    templates[event.rawSlot]?.handle(event, this, *args)
+                }
+            }
+        }
+    }
+
+}
